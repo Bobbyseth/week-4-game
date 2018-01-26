6d3fd3ab9fa72f44d1868e1f2049a09c3763c479
Bobbyseth <seth.griggs007@gmail.com>
2018-01-25 19:48:05 -0600

    Final update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,40 +2,62 @@
 //make sure everything is loaded.
 $(document).ready(function(){
 //Define our vars here:
-var totalScore; // equals the accumalation of gem clicks, compared to computerNum to see if win / loss.
-var gem1; // gets assigned a random number from 1-20.
-var gem2; // gets assigned a random number from 10-20.
-var gem3; // gets assigned a random number from 1-10.
-var gem4; // gets assigned a random number from 5-15.
-var wins; // each time totalScore === randomNbr, this gets ++
-var losses; // each tome totalScore > randomNbr, this gets --
-var minNumber = 25; // range for randomNbr
-var maxNumber = 75; // range for randomNbr
-var computerNum = computerNumFromRange(minNumber, maxNumber);
+var gem1 = Math.floor(Math.random() * 10) + 1; // gem values listed here.
+var gem2 = Math.floor(Math.random() * 6) + 10;
+var gem3 = Math.floor(Math.random() * 6) + 15;
+var gem4 = Math.floor(Math.random() * 6) + 20;
+var computerNum = Math.floor(Math.random() * 60) + 25; // our random computer generated number to compare against.
+var wins = 0; // each time totalScore === randomNbr, this gets ++
+var losses = 0; // each tome totalScore > randomNbr, this gets --
+var totalScore = 0; // equals the accumalation of gem clicks, compared to computerNum to see if win / loss.
+//console.log our numbers here:
+console.log(gem1);
+console.log(gem2);
+console.log(gem3);
+console.log(gem4);
+console.log(totalScore);
 
-//functions here:
-function reset() {
-  computerNumFromRange();
-  totalScore = 0;
-  getGem1();
-  getGem2();
-  getGem3();
-  getGem4();
+
+function check() { // each time you click a gem, this gets run.
+  if (totalScore === computerNum) {
+    alert("You Win!");
+    wins++;
+    }
+  if (totalScore > computerNum) {
+    alert("You lose...");
+    losses++;
+  }
 }
 
-function computerNumFromRange(min,max) {
-  return Math.floor(Math.random()*(max-min+1)+min);
-}
+
+
+//button logic down here: On click of button, labled gem1-gem4, gets value and adds to totalScore and then runs check().
+$("#gemOne").click(function(){
+  console.log("totalScore " + totalScore);
+  totalScore = totalScore + gem1;
+  check();
+});
+$("#gemTwo").click(function(){
+  console.log("totalScore " + totalScore);
+  totalScore = totalScore + gem2;
+  check();
+});
+$("#gemThree").click(function(){
+  console.log("totalScore " + totalScore);
+  totalScore = totalScore + gem3;
+  check();
+});
+$("#gemFour").click(function(){
+  console.log("totalScore " + totalScore);
+  totalScore = totalScore + gem4;
+  check();
+});
+
+
+
 //show values on UI
 document.querySelector("#randomNbr").innerHTML = computerNum;
 document.querySelector("#totScore").innerHTML = totalScore;
 document.querySelector("#won").innerHTML = wins;
 document.querySelector("#loss").innerHTML = losses;
-// we also need to assign random numbers for each of the crystals, thinking 1-5, 5-10, 10-15, 15-20, so that we make sure we get some good range there. If not, I can see maybe having one 20, one 19, and a 1, and maybe a 3.  that would suck.
-//those numbers need to be assigned to their buttons.
-// the buttons, when clicked, need to add themselved up.
-//the added number is put into a var that keeps track.
-// that var get compared against the randomNbr, if it goes above, loss++ and reset, if it matches win++ and reset.
-
-
 });
